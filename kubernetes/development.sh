4eb61ea32bd45d700451b99fdc63027b14f574e7
Anders Larsen <anders.l.h.larsen@live.dk>
2021-08-20 10:54:44 +0000

    moved k apply back to workflow

--- kubernetes/development.sh
+++ kubernetes/development.sh
@@ -16,9 +16,9 @@ template5=`cat "redis-service.yaml.template" | sed "s/{{NAME}}/$DB_NAME/g"`
 template6=`cat "redis-deployment.yaml.template" | sed "s/{{NAME}}/$DB_NAME/g"`
 
 # apply the yml with the substituted value
-echo "$template1" | kubectl apply -f -
-echo "$template2" | kubectl apply -f -
-echo "$template3" | kubectl apply -f -
-echo "$template4" | kubectl apply -f -
-echo "$template5" | kubectl apply -f -
-echo "$template6" | kubectl apply -f -
+echo "$template1" > ./services/frontend-service.yaml
+echo "$template2" > ./deployments/frontend-deployment.yaml
+echo "$template3" > ./services/backend-service.yaml
+echo "$template4" > ./deployments/backend-deployment.yaml
+echo "$template5" > ./services/redis-service.yaml
+echo "$template6" > ./deployments/redis-deployment.yaml
