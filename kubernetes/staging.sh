0272da1a70645f8cccbb316b5ad9b667f99c1c15
Anders Larsen <anders.l.h.larsen@live.dk>
2021-08-20 10:43:19 +0000

    changed names of deployments to mach environment

--- kubernetes/staging.sh
+++ kubernetes/staging.sh
@@ -1,11 +1,22 @@
-#!/bin/bash
-
 # sample value for your variables
 PORT="31376"
+FRONTEND_NAME="staging-frontend"
+BACKEND_NAME="staging-backend"
+DB_NAME="staging-redis"
 
 # read the yml template from a file and substitute the string 
 # {{MYVARNAME}} with the value of the MYVARVALUE variable
-template=`cat "frontend-service.yaml.template" | sed "s/{{PORT}}/$PORT/g"`
+template1=`cat "frontend-service.yaml.template" | sed "s/{{PORT}}/$PORT/g" | sed "s/{{NAME}}/$FRONTEND_NAME/g"` 
+template2=`cat "frontend-deployment.yaml.template" | sed "s/{{NAME}}/$FRONTEND_NAME/g"`
+template3=`cat "backend-service.yaml.template" | sed "s/{{NAME}}/$BACKEND_NAME/g"`
+template4=`cat "backend-deployment.yaml.template" | sed "s/{{NAME}}/$BACKEND_NAME/g"`
+template5=`cat "redis-service.yaml.template" | sed "s/{{NAME}}/$DB_NAME/g"`
+template6=`cat "redis-deployment.yaml.template" | sed "s/{{NAME}}/$DB_NAME/g"`
 
 # apply the yml with the substituted value
-echo "$template" > frontend-service.yaml
+echo "$template1" > frontend-service.yaml
+echo "$template2" > frontend-deployment.yaml
+echo "$template3" > backend-service.yaml
+echo "$template4" > backend-deployment.yaml
+echo "$template5" > redis-service.yaml
+echo "$template6" > redis-deployment.yaml
