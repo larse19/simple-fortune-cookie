ab970951c3c6d0d99394121ddc26599f926d2da7
Anders Larsen <anders.l.h.larsen@live.dk>
2021-08-20 09:57:02 +0000

    scripts for setting ports

---
+++ kubernetes/staging.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# sample value for your variables
+PORT="31376"
+
+# read the yml template from a file and substitute the string 
+# {{MYVARNAME}} with the value of the MYVARVALUE variable
+template=`cat "frontend-service.yaml.template" | sed "s/{{PORT}}/$PORT/g"`
+
+# apply the yml with the substituted value
+echo "$template" > frontend-service.yaml
