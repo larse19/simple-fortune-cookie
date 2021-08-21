b430f222d2b66ea686b2692b497dbae38cb0d61e
Theis Tengs <theistengs@gmail.com>
2021-08-21 14:45:40 +0000

    Staging.sh file edited

--- kubernetes/staging.sh
+++ kubernetes/staging.sh
@@ -7,9 +7,9 @@ DB_NAME="staging-redis"
 # read the yml template from a file and substitute the string 
 # {{MYVARNAME}} with the value of the MYVARVALUE variable
 template1=`cat "frontend-service.yaml.template" | sed "s/{{PORT}}/$PORT/g" | sed "s/{{NAME}}/$FRONTEND_NAME/g"` 
-template2=`cat "frontend-deployment.yaml.template" | sed "s/{{NAME}}/$FRONTEND_NAME/g"`
+template2=`cat "frontend-deployment.yaml.template" | sed "s/{{NAME}}/$FRONTEND_NAME/g" | sed "s/{{BACKEND_NAME}}/$BACKEND_NAME/g"`
 template3=`cat "backend-service.yaml.template" | sed "s/{{NAME}}/$BACKEND_NAME/g"`
-template4=`cat "backend-deployment.yaml.template" | sed "s/{{NAME}}/$BACKEND_NAME/g"`
+template4=`cat "backend-deployment.yaml.template" | sed "s/{{NAME}}/$BACKEND_NAME/g" | sed "s/{{REDIS_NAME}}/$DB_NAME/g"`
 template5=`cat "redis-service.yaml.template" | sed "s/{{NAME}}/$DB_NAME/g"`
 template6=`cat "redis-deployment.yaml.template" | sed "s/{{NAME}}/$DB_NAME/g"`
 
